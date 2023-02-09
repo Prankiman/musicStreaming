5dc4451cd441a3003d812e85e4c4d288cb319c7b
jonathan <jonathan@pop-os.localdomain>
2023-02-09 16:00:23 +0100

    added firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +15,14 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          colorSchemeSeed: Color.fromARGB(255, 224, 0, 0), useMaterial3: true),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('musicStreaming'),
+          foregroundColor: Color.fromARGB(255, 213, 213, 213),
+          backgroundColor: Color.fromARGB(255, 50, 50, 50),
+        ),
         body: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
